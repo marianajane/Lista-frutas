1344603f5de894cc12400166d28be1ca7542cdbb
marianajane <mariana.musm@gmail.com>
2017-03-20 21:57:14 -0600

    Html y JS

---
+++ js/javascript.js.js
@@ -0,0 +1,14 @@
+function agregarFruta() {
+    var frutas = [manzana, naranja];
+
+    this.add = add;
+    this.size = size;
+
+    function add(element) {
+      document.getElementById frutas.push(frutas);
+    }
+
+    function size() {
+      document.getElementById (frutas.length);
+    }
+}
